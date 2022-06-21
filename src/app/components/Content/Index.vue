1c9f2c3fa9bb7ebd0c025983d9a746730aa297cc
zehua <zehua@zehuadeiMac.local>
2022-06-22 00:06:06 +0800

    添加登录

--- src/app/components/Content/Index.vue
+++ src/app/components/Content/Index.vue
@@ -1,131 +1,34 @@
 <!--
  * @Author: zeHua
- * @Date: 2022-06-21 11:15:09
+ * @Date: 2022-06-21 21:42:41
  * @LastEditors: zeHua
- * @LastEditTime: 2022-06-21 18:40:37
- * @FilePath: \sticky-notes\src\app\components\Content\Index.vue
+ * @LastEditTime: 2022-06-21 21:59:32
+ * @FilePath: /wintao/wintao-daily/src/app/components/Content/Index.vue
 -->
 <template>
-  <n-space vertical>
-    <n-input
-      v-model:value="data.useraccount"
-      type="text"
-      placeholder="请输入账号"
-    />
-
-    <n-input
-      v-model:value="data.password"
-      type="password"
-      placeholder="请输入密码"
-    />
-    <n-grid x-gap="12" :cols="4">
-      <n-gi>
-        <n-input
-          v-model:value="data.code"
-          type="text"
-          placeholder="请输入验证码"
-        />
-      </n-gi>
-      <n-gi @click="getImageUrl">
-        <img :src="data.imageUrl" />
-      </n-gi>
-    </n-grid>
-
-    <n-button @click="handleLogin">登录</n-button>
-  </n-space>
+  <div class="content">
+    <div class="zw" />
+    <TodoList />
+    <div class="zw" />
+  </div>
 </template>
 
-<script lang="ts">
-import { defineComponent, onMounted, reactive, ref } from "vue";
-import { NButton, NInput, useMessage } from "naive-ui";
-import axios from "axios";
-export default defineComponent({
-  components: {
-    NButton,
-    NInput,
-  },
-  setup() {
-    const message = useMessage();
-
-    const data = reactive({
-      useraccount: "", // 用户名
-      password: "", //用户密码
-      code: "", // 验证码
-      imageUrl: "", // 图片路径
-    });
-    function handleLogin() {
-      console.log(getCookie("JSESSIONID"));
-      axios.defaults.withCredentials = true;
-      axios({
-        url: "http://192.168.0.12:8080/WDMS/system/login",
-        method: "POST",
-        data: `useraccount=${data.useraccount}&password=${data.password}&code=${data.code}`,
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded",
-          Cookies: getCookie("JSESSIONID"),
-        },
-      }).then((response) => {
-        let data = response.data;
-        if (data.code !== 200) {
-          message.warning(data.message);
-        }
-      });
-      //   axios
-      //     .post("http://192.168.0.12:8080/WDMS/system/login", {
-      //       useraccount: data.useraccount,
-      //       password: data.password,
-      //       code: data.code,
-      //     })
-      //     .then(function (response) {
-      //       console.log(response);
-      //       let data = response.data;
-      //       if (data.code !== 200) {
-      //         message.warning(data.message);
-      //       }
-      //     })
-      //     .catch(function (error) {
-      //       console.log(error);
-      //     });
-    }
-
-    // 获取图片路径
-    function getImageUrl() {
-      data.imageUrl =
-        "http://192.168.0.12:8080/WDMS/system/checkCode?a=" + new Date();
-    }
-
-    function getCookie(cookie_name) {
-      var allcookies = document.cookie;
-      //索引长度，开始索引的位置
-      var cookie_pos = allcookies.indexOf(cookie_name);
-
-      // 如果找到了索引，就代表cookie存在,否则不存在
-      if (cookie_pos != -1) {
-        // 把cookie_pos放在值的开始，只要给值加1即可
-        //计算取cookie值得开始索引，加的1为“=”
-        cookie_pos = cookie_pos + cookie_name.length + 1;
-        //计算取cookie值得结束索引
-        var cookie_end = allcookies.indexOf(";", cookie_pos);
-
-        if (cookie_end == -1) {
-          cookie_end = allcookies.length;
-        }
-        //得到想要的cookie的值
-        var value = unescape(allcookies.substring(cookie_pos, cookie_end));
-      }
-      return value;
-    }
-
-    onMounted(() => {
-      document.cookie = "JSESSIONID=68AEDC33545F0374A97421F279D9F8FF";
-      getImageUrl();
-    });
-
-    return {
-      data,
-      handleLogin,
-      getImageUrl,
-    };
-  },
-});
+<script setup lang="ts">
+import { inject, onMounted, provide } from 'vue'
+import TodoList from './components/TodoList'
 </script>
+
+<style lang="less" scoped>
+.content {
+  // position: absolute;
+  // left: 0;
+  // top: 40px;
+  // bottom: 0px;
+  // width: 100%;
+  width: 100%;
+  height: 100%;
+  .zw {
+    height: 40px;
+  }
+}
+</style>
--- src/app/components/Setting/Login/index.vue
+++ src/app/components/Setting/Login/index.vue
@@ -1,5 +1,48 @@
+<!--
+ * @Author: zeHua
+ * @Date: 2022-06-21 11:15:09
+ * @LastEditors: zeHua
+ * @LastEditTime: 2022-06-21 23:53:31
+ * @FilePath: /wintao/wintao-daily/src/app/components/Setting/Login/index.vue
+-->
 <template>
-  <n-space vertical>
+  <n-form ref="formRef" :model="model" :rules="rules">
+    <n-form-item path="age" label="用户名">
+      <n-input v-model:value="model.username" @keydown.enter.prevent />
+    </n-form-item>
+    <n-form-item path="password" label="密码">
+      <n-input
+        v-model:value="model.password"
+        type="password"
+        @input="handlePasswordInput"
+        @keydown.enter.prevent
+      />
+    </n-form-item>
+    <n-form-item path="code" label="验证码">
+      <n-input
+        v-model:value="model.code"
+        type="password"
+        @input="handlePasswordInput"
+        @keydown.enter.prevent
+      />
+    </n-form-item>
+
+    <n-row>
+      <n-col>
+        <div>
+          <n-button
+            round
+            type="primary"
+            @click="handleValidateButtonClick"
+            style="width: 100%"
+          >
+            登 录
+          </n-button>
+        </div>
+      </n-col>
+    </n-row>
+  </n-form>
+  <!-- <n-space vertical>
     <n-input
       v-model:value="data.useraccount"
       type="text"
@@ -7,7 +50,7 @@
     />
 
     <n-input
-      v-model:value="data.passwold"
+      v-model:value="data.password"
       type="password"
       placeholder="请输入密码"
     />
@@ -19,81 +62,197 @@
           placeholder="请输入验证码"
         />
       </n-gi>
-      <n-gi>
+      <n-gi @click="getImageUrl">
         <img :src="data.imageUrl" />
       </n-gi>
     </n-grid>
 
     <n-button @click="handleLogin">登录</n-button>
-  </n-space>
+  </n-space> -->
 </template>
 
 <script lang="ts">
-import { defineComponent, onMounted, reactive, ref } from "vue";
-import { NButton, NInput, useMessage } from "naive-ui";
-import axios from "axios";
+// import { defineComponent, onMounted, reactive, ref } from 'vue'
+// import { NButton, NInput, useMessage } from 'naive-ui'
+// import axios from 'axios'
+// export default defineComponent({
+//   components: {
+//     NButton,
+//     NInput,
+//   },
+//   setup() {
+//     const message = useMessage()
+
+//     const data = reactive({
+//       useraccount: '', // 用户名
+//       password: '', //用户密码
+//       code: '', // 验证码
+//       imageUrl: '', // 图片路径
+//     })
+//     function handleLogin() {
+//       console.log(getCookie('JSESSIONID'))
+//       axios.defaults.withCredentials = true
+//       axios({
+//         url: 'http://192.168.0.12:8080/WDMS/system/login',
+//         method: 'POST',
+//         data: `useraccount=${data.useraccount}&password=${data.password}&code=${data.code}`,
+//         headers: {
+//           'Content-Type': 'application/x-www-form-urlencoded',
+//           Cookies: getCookie('JSESSIONID'),
+//         },
+//       }).then(response => {
+//         let data = response.data
+//         if (data.code !== 200) {
+//           message.warning(data.message)
+//         }
+//       })
+//       //   axios
+//       //     .post("http://192.168.0.12:8080/WDMS/system/login", {
+//       //       useraccount: data.useraccount,
+//       //       password: data.password,
+//       //       code: data.code,
+//       //     })
+//       //     .then(function (response) {
+//       //       console.log(response);
+//       //       let data = response.data;
+//       //       if (data.code !== 200) {
+//       //         message.warning(data.message);
+//       //       }
+//       //     })
+//       //     .catch(function (error) {
+//       //       console.log(error);
+//       //     });
+//     }
+
+//     // 获取图片路径
+//     function getImageUrl() {
+//       data.imageUrl =
+//         'http://192.168.0.12:8080/WDMS/system/checkCode?a=' + new Date()
+//     }
+
+//     function getCookie(cookie_name) {
+//       var allcookies = document.cookie
+//       //索引长度，开始索引的位置
+//       var cookie_pos = allcookies.indexOf(cookie_name)
+
+//       // 如果找到了索引，就代表cookie存在,否则不存在
+//       if (cookie_pos != -1) {
+//         // 把cookie_pos放在值的开始，只要给值加1即可
+//         //计算取cookie值得开始索引，加的1为“=”
+//         cookie_pos = cookie_pos + cookie_name.length + 1
+//         //计算取cookie值得结束索引
+//         var cookie_end = allcookies.indexOf(';', cookie_pos)
+
+//         if (cookie_end == -1) {
+//           cookie_end = allcookies.length
+//         }
+//         //得到想要的cookie的值
+//         var value = unescape(allcookies.substring(cookie_pos, cookie_end))
+//       }
+//       return value
+//     }
+
+//     onMounted(() => {
+//       document.cookie = 'JSESSIONID=68AEDC33545F0374A97421F279D9F8FF'
+//       getImageUrl()
+//     })
+
+//     return {
+//       data,
+//       handleLogin,
+//       getImageUrl,
+//     }
+//   },
+// })
+
+import { defineComponent, ref } from 'vue'
+import { useMessage, NForm, NFormItem, NInput, NButton } from 'naive-ui'
+
 export default defineComponent({
   components: {
-    NButton,
+    NForm,
+    NFormItem,
     NInput,
+    NButton,
   },
   setup() {
-    const message = useMessage();
-
-    const data = reactive({
-      useraccount: "", // 用户名
-      passwold: "", //用户密码
-      code: "", // 验证码
-      imageUrl: "", // 图片路径
-    });
-    function handleLogin() {
-      alert(data.code);
-      axios({
-        url: "http://192.168.0.12:8080/WDMS/system/login",
-        data: {
-          useraccount: data.useraccount,
-          passwold: data.passwold,
-          code: data.code,
+    const formRef = ref(null)
+    const rPasswordFormItemRef = ref(null)
+    const message = useMessage()
+    const modelRef = ref({
+      username: null,
+      password: null,
+      code: null,
+    })
+    function validatePasswordStartWith(rule, value) {
+      return (
+        !!modelRef.value.password &&
+        modelRef.value.password.startsWith(value) &&
+        modelRef.value.password.length >= value.length
+      )
+    }
+    function validatePasswordSame(rule, value) {
+      return value === modelRef.value.password
+    }
+    const rules = {
+      age: [
+        {
+          required: true,
+          message: '请输入用户名',
         },
-        headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      }).then((response) => {
-        let data = response.data;
-        if (data.code !== 200) {
-          message.warning(data.message);
-        }
-      });
-      //   axios
-      //     .post("http://192.168.0.12:8080/WDMS/system/login", {
-      //       useraccount: data.useraccount,
-      //       passwold: data.passwold,
-      //       code: data.code,
-      //     })
-      //     .then(function (response) {
-      //       console.log(response);
-      //       let data = response.data;
-      //       if (data.code !== 200) {
-      //         message.warning(data.message);
-      //       }
-      //     })
-      //     .catch(function (error) {
-      //       console.log(error);
-      //     });
+      ],
+      password: [
+        {
+          required: true,
+          message: '请输入密码',
+        },
+      ],
+      code: [
+        {
+          required: true,
+          message: '请输入验证码',
+        },
+      ],
+      // reenteredPassword: [
+      //   {
+      //     required: true,
+      //     message: '请再次输入密码',
+      //     trigger: ['input', 'blur'],
+      //   },
+      //   {
+      //     validator: validatePasswordStartWith,
+      //     message: '两次密码输入不一致',
+      //     trigger: 'input',
+      //   },
+      //   {
+      //     validator: validatePasswordSame,
+      //     message: '两次密码输入不一致',
+      //     trigger: ['blur', 'password-input'],
+      //   },
+      // ],
     }
-
-    // 获取图片路径
-    function getImageUrl() {
-      data.imageUrl =
-        "http://192.168.0.12:8080/WDMS/system/checkCode?a=" + new Date();
-    }
-
-    onMounted(() => {
-      getImageUrl();
-    });
-
     return {
-      data,
-      handleLogin,
-    };
+      formRef,
+      rPasswordFormItemRef,
+      model: modelRef,
+      rules,
+      handlePasswordInput() {
+        if (modelRef.value.reenteredPassword) {
+          rPasswordFormItemRef.value?.validate({ trigger: 'password-input' })
+        }
+      },
+      handleValidateButtonClick(e) {
+        e.preventDefault()
+        formRef.value?.validate(errors => {
+          if (!errors) {
+            message.success('验证成功')
+          } else {
+            console.log(errors)
+            message.error('验证失败')
+          }
+        })
+      },
+    }
   },
-});
+})
 </script>
