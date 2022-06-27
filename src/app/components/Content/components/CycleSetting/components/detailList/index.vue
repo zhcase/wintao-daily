c0c3d25698cba64e8c59673d3bd27c83e0e17c5c
zehua <zehua@zehuadeiMac.local>
2022-06-27 21:07:58 +0800

    添加详情页面

--- src/app/components/Content/components/CycleSetting/components/detailList/index.vue
+++ src/app/components/Content/components/CycleSetting/components/detailList/index.vue
@@ -2,9 +2,93 @@
  * @Author: zeHua
  * @Date: 2022-06-27 18:25:50
  * @LastEditors: zeHua
- * @LastEditTime: 2022-06-27 18:33:47
- * @FilePath: \sticky-notes\src\app\components\Content\components\CycleSetting\components\detailList\index.vue
+ * @LastEditTime: 2022-06-27 20:56:29
+ * @FilePath: /wintao/wintao-daily/src/app/components/Content/components/CycleSetting/components/detailList/index.vue
 -->
 <template>
-  <div>2323</div>
+  <div class="daily">
+    <el-collapse v-model="activeNames" @change="handleChange">
+      <el-collapse-item title=" IDMP智慧数字管理平台(3.5h)" name="1">
+        <el-descriptions direction="vertical" :column="4" size="small" border>
+          <el-descriptions-item label="姓名">陈泽华</el-descriptions-item>
+          <el-descriptions-item label="功能名称">功能开发</el-descriptions-item>
+          <el-descriptions-item label="时长(h)">7.5</el-descriptions-item>
+          <el-descriptions-item label="申报日期"
+            >2022-06-01</el-descriptions-item
+          >
+          <el-descriptions-item label="备注" :span="4">
+            No.1188, Wuzhong Avenue, Wuzhong District, Suzhou, Jiangsu Province
+          </el-descriptions-item>
+          <el-descriptions-item label="操作">
+            <el-button type="primary" :icon="Edit" circle />
+            <el-button type="danger" :icon="Delete" circle />
+          </el-descriptions-item>
+        </el-descriptions>
+      </el-collapse-item>
+      <el-collapse-item title="Feedback" name="2">
+        <div>
+          Operation feedback: enable the users to clearly perceive their
+          operations by style updates and interactive effects;
+        </div>
+        <div>
+          Visual feedback: reflect current state by updating or rearranging
+          elements of the page.
+        </div>
+      </el-collapse-item>
+      <el-collapse-item title="Efficiency" name="3">
+        <div>
+          Simplify the process: keep operating process simple and intuitive;
+        </div>
+        <div>
+          Definite and clear: enunciate your intentions clearly so that the
+          users can quickly understand and make decisions;
+        </div>
+        <div>
+          Easy to identify: the interface should be straightforward, which helps
+          the users to identify and frees them from memorizing and recalling.
+        </div>
+      </el-collapse-item>
+      <el-collapse-item title="Controllability" name="4">
+        <div>
+          Decision making: giving advices about operations is acceptable, but do
+          not make decisions for the users;
+        </div>
+        <div>
+          Controlled consequences: users should be granted the freedom to
+          operate, including canceling, aborting or terminating current
+          operation.
+        </div>
+      </el-collapse-item>
+    </el-collapse>
+  </div>
 </template>
+<script lang="ts" setup>
+import { ref } from 'vue'
+import {
+  Check,
+  Delete,
+  Edit,
+  Message,
+  Search,
+  Star,
+} from '@element-plus/icons-vue'
+const activeNames = ref(['1'])
+const size = ref('samll')
+
+const handleChange = (val: string[]) => {
+  console.log(val)
+}
+</script>
+
+<style lang="less" scoped>
+.daily {
+  position: fixed;
+  top: 40px;
+  left: 0;
+  z-index: 9999;
+  width: 100%;
+  height: calc(100% - 40px);
+  border: 1px solid red;
+  background-color: #fff;
+}
+</style>
--- src/app/pages/Home/Index.vue
+++ src/app/pages/Home/Index.vue
@@ -9,15 +9,13 @@
       <NConfigProvider :locale="zhCN" :date-locale="dateZhCN" ref="parentRefs">
         <TopNav v-model:lock="lock" />
         <!-- <Content /> -->
-        <Login @handleLogin="handleLogin" v-if="!isLogin" />
-        <PageCalendar
-          @updateDate="handleChangeDate"
-          ref="calendarRefs"
-          v-if="isLogin"
-        />
+        <!-- <Login @handleLogin="handleLogin" v-if="!isLogin" /> -->
+        <!-- v-if="isLogin" -->
+
+        <PageCalendar @updateDate="handleChangeDate" ref="calendarRefs" />
+        <!-- v-if="isShowWorkForm" -->
 
         <CycleSetting
-          v-if="isShowWorkForm"
           :currentDate="selectCurrentDate"
           :workContent="workContent"
           @update:show="isShowWorkForm = false"
@@ -30,70 +28,70 @@
 </template>
 
 <script setup lang="ts">
-import { NMessageProvider, NConfigProvider, zhCN, dateZhCN } from "naive-ui";
-import TopNav from "@app/components/TopNav";
-import Content from "@app/components/Content";
-import BottomNav from "@app/components/BottomNav";
-import { onMounted, Ref, ref, watch, getCurrentInstance } from "vue";
-import { leaveToZero, moveToZero } from "@/app/utils/event";
-import remote from "@/app/utils/render";
-import Login from "@/app/components/Setting/Login";
-import PageCalendar from "@/app/components/Calendar"; //选择日期
-import CycleSetting from "@/app/components/Content/components/CycleSetting";
-import { ElNotification } from "element-plus";
+import { NMessageProvider, NConfigProvider, zhCN, dateZhCN } from 'naive-ui'
+import TopNav from '@app/components/TopNav'
+import Content from '@app/components/Content'
+import BottomNav from '@app/components/BottomNav'
+import { onMounted, Ref, ref, watch, getCurrentInstance } from 'vue'
+import { leaveToZero, moveToZero } from '@/app/utils/event'
+import remote from '@/app/utils/render'
+import Login from '@/app/components/Setting/Login'
+import PageCalendar from '@/app/components/Calendar' //选择日期
+import CycleSetting from '@/app/components/Content/components/CycleSetting'
+import { ElNotification } from 'element-plus'
 
-const win = remote.getCurrentWindow();
-console.log(window);
+const win = remote.getCurrentWindow()
+console.log(window)
 
-win.setIgnoreMouseEvents(false);
+win.setIgnoreMouseEvents(false)
 
 // 锁定
-const lock = ref(false);
-const isShowWorkForm = ref(false); //是否显示工作表单
-const selectCurrentDate = ref(null); //选中的当前日期
-const calendarRefs = ref(""); //
-const workContent = ref(""); // 工作内容
-const parentRefs = ref("");
-watch(lock, (newV) => {
+const lock = ref(false)
+const isShowWorkForm = ref(false) //是否显示工作表单
+const selectCurrentDate = ref(null) //选中的当前日期
+const calendarRefs = ref('') //
+const workContent = ref('') // 工作内容
+const parentRefs = ref('')
+watch(lock, newV => {
   if (newV) {
     if (intervalId) {
-      clearInterval(intervalId);
-      intervalId = null;
+      clearInterval(intervalId)
+      intervalId = null
     }
   } else {
     if (win.getPosition()[1] <= 0) {
-      initInfo();
+      initInfo()
     }
   }
-});
+})
 
 // 隐藏
-const leave = ref(false);
-const isLogin = ref(false);
+const leave = ref(false)
+const isLogin = ref(false)
 
-let intervalId;
+let intervalId
 const handleLogin = () => {
-  isLogin.value = true;
-};
+  isLogin.value = true
+}
 
 // 选择完成日期
-const handleChangeDate = (data) => {
-  isShowWorkForm.value = true;
+const handleChangeDate = data => {
+  isShowWorkForm.value = true
 
-  workContent.value = (calendarRefs.value as any).workContent;
+  workContent.value = (calendarRefs.value as any).workContent
 
-  selectCurrentDate.value = data.day;
-};
+  selectCurrentDate.value = data.day
+}
 const checkShowOrHidden = () => {
-  const cursorPos = remote.screen.getCursorScreenPoint();
+  const cursorPos = remote.screen.getCursorScreenPoint()
   if (!leave.value) {
     if (
       cursorPos.x < posXMin ||
       cursorPos.x > posXMax ||
       cursorPos.y > posYMax
     ) {
-      leave.value = true;
-      win.setIgnoreMouseEvents(true, { forward: true });
+      leave.value = true
+      win.setIgnoreMouseEvents(true, { forward: true })
     }
   } else {
     if (
@@ -101,45 +99,45 @@ const checkShowOrHidden = () => {
       cursorPos.x <= posXMax &&
       cursorPos.y < posYMaxHidden
     ) {
-      leave.value = false;
-      remote.getCurrentWindow().setIgnoreMouseEvents(false);
+      leave.value = false
+      remote.getCurrentWindow().setIgnoreMouseEvents(false)
     }
   }
-};
+}
 const refreshWorkTable = () => {
-  (calendarRefs.value as any).getDaily();
-};
+  ;(calendarRefs.value as any).getDaily()
+}
 
 // 是否可以贴边隐藏
-const canHidden = ref(true);
-let posXMin = 0;
-let posXMax = 0;
-let posYMin = win.getPosition()[1];
-let posYMax = 0;
-let posYMaxHidden = 2;
+const canHidden = ref(true)
+let posXMin = 0
+let posXMax = 0
+let posYMin = win.getPosition()[1]
+let posYMax = 0
+let posYMaxHidden = 2
 const initInfo = () => {
-  let size = win.getSize();
-  posXMin = win.getPosition()[0];
-  posXMax = posXMin + size[0];
-  posYMin = 0;
-  posYMax = size[1];
-  canHidden.value = true;
-  win.setResizable(false);
-  if (!intervalId) intervalId = setInterval(checkShowOrHidden, 300);
-};
-if (posYMin <= 0) initInfo();
-moveToZero(initInfo);
+  let size = win.getSize()
+  posXMin = win.getPosition()[0]
+  posXMax = posXMin + size[0]
+  posYMin = 0
+  posYMax = size[1]
+  canHidden.value = true
+  win.setResizable(false)
+  if (!intervalId) intervalId = setInterval(checkShowOrHidden, 300)
+}
+if (posYMin <= 0) initInfo()
+moveToZero(initInfo)
 leaveToZero(() => {
-  win.setResizable(true);
-  canHidden.value = false;
+  win.setResizable(true)
+  canHidden.value = false
   if (intervalId) {
-    clearInterval(intervalId);
-    intervalId = null;
+    clearInterval(intervalId)
+    intervalId = null
   }
-});
+})
 onMounted(() => {
-  isLogin.value = localStorage.getItem("sessionId") ? true : false;
-});
+  isLogin.value = localStorage.getItem('sessionId') ? true : false
+})
 </script>
 
 <style lang="less" scoped>
